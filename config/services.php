c771f1c569a1cd66c05df2299838dec9f85b2ebb
Cyril de Wit <cyrilwit@gmail.com>
2018-10-25 20:45:31 +0200

    feat: update uses of old User model namespace

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => App\Domain\Users\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'webhook' => [
