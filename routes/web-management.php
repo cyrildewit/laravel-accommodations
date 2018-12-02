75673d63ee5c19870eeea866fd397fe5effd4122
Cyril de Wit <cyrilwit@gmail.com>
2018-12-02 16:13:38 +0100

    feat(management): add missing auth routes

--- routes/web-management.php
+++ routes/web-management.php
@@ -1,10 +1,31 @@
 <?php
 
-Route::namespace('Auth')->name('auth.')->group(function () {
-    Route::get('login', 'LoginController@showLoginForm')->name('login');
-    Route::post('login', 'LoginController@login');
+Route::middleware('auth:management', 'can:browse_management')->group(function () {
+
+    // Dashboard
+    Route::get('dashboard')->name('dashboard');
+
 });
 
-Route::middleware('auth:management', 'can:browse_management')->group(function () {
-    Route::get('dashboard')->name('dashboard');
+Route::namespace('Auth')->name('auth.')->group(function () {
+
+    // Authentication routes
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('logout');
+
+    // Registration routes
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register');
+
+    // Password reset routes
+    Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.update');
+
+    // Email verification routes
+    Route::get('email/verify', 'VerificationController@showLinkRequestForm')->name('verification.notice');
+    Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
+    Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
 });
