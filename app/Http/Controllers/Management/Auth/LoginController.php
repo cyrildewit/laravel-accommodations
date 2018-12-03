baeea7970c66b47e616c652c0beecc3bcd874d14
Cyril de Wit <cyrilwit@gmail.com>
2018-12-03 15:26:38 +0100

    feat: add login views and update paths in controllers

--- app/Http/Controllers/Management/Auth/LoginController.php
+++ app/Http/Controllers/Management/Auth/LoginController.php
@@ -21,6 +21,16 @@ class LoginController extends Controller
     }
 
     /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('management.auth.login');
+    }
+
+    /**
      * Where to redirect users after login.
      *
      * @return string
---
+++ resources/views/portal/auth/login.blade.php
@@ -0,0 +1,1 @@
+<h1>Login page for Portal</h1>
