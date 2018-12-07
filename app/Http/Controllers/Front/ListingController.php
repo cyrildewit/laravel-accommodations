62b5f06c78c30b764c22f3a4bf54b35053bec16b
Cyril de Wit <cyrilwit@gmail.com>
2018-12-07 22:15:48 +0100

    feat: keep the routes simple for now

--- app/Http/Controllers/Front/ListingController.php
+++ app/Http/Controllers/Front/ListingController.php
@@ -8,9 +8,9 @@ use App\Domain\Listings\Models\Listing;
 
 class ListingController extends Controller
 {
-    public function show($listingType, $areaSlug, $listingSlug, $slugId)
+    public function show($id)
     {
-        $listing = Listing::where('slug_id', $slugId)->first();
+        $listing = Listing::where('id', $id)->first();
 
         return view('front.listing.show', [
             'listing' => $listing,
--- routes/web-front.php
+++ routes/web-front.php
@@ -2,4 +2,5 @@
 
 Route::get('/', 'HomeController@index')->name('home.index');
 Route::get('/search', 'SearchController@index')->name('search.index');
-Route::get('/{listingType}/{areaSlug}/{listingSlug}/{id}', 'ListingController@show')->name('listing.show');
+Route::get('/listings/{id}', 'ListingController@show')->name('listing.show');
+// Route::get('/{listingType}/{areaSlug}/{listingSlug}/{id}', 'ListingController@show')->name('listing.show');
