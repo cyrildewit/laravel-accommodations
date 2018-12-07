e57a14645e6b13dc32d4be6c98997bfe962c61dc
Cyril de Wit <cyrilwit@gmail.com>
2018-12-07 15:06:36 +0100

    feat: add translation file for enums

---
+++ resources/lang/en/enums.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Domain\Listings\Enums\ListingType;
+
+return [
+
+    // ListingType::class => [
+    //     UserType::Administrator => 'Administrator',
+    //     UserType::SuperAdministrator => 'Super administrator',
+    // ],
+
+];
