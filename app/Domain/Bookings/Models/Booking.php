02ed1afcb4889169ffa4b5ebb7537609c09cb1c7
Cyril de Wit <cyrilwit@gmail.com>
2018-12-08 21:43:31 +0100

    feat: add booking model

---
+++ app/Domain/Bookings/Models/Booking.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Domain\Bookings\Models;
+
+use App\Domain\Users\Models\User;
+use App\Domain\Rooms\Models\Room;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Booking extends Model
+{
+    // /**
+    //  * Get the listing.
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+    //  */
+    // public function listing(): MorphTo
+    // {
+    //     return $this->belongsTo(Listing::class);
+    // }
+
+    /**
+     * Get the room.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    /**
+     * Get the user who booked this booking.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
