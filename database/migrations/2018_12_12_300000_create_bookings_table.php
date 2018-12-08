c1b010069551f60c519cbaefd6fbffc882322aa8
Cyril de Wit <cyrilwit@gmail.com>
2018-12-08 21:46:12 +0100

    feat: add create bookings table migration

---
+++ database/migrations/2018_12_12_300000_create_bookings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            // $table->integer('listing_id')->unsigned();
+            $table->integer('room_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('reference_id')->unique();
+            $table->date('check_in_date');
+            $table->date('check_out_date');
+            $table->integer('number_of_guests');
+            $table->integer('total_price');
+            $table->timestamps();
+
+            //$table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
