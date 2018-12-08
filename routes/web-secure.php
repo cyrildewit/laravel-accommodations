eb20234deee23d26d60ae04140be0a518965ba28
Cyril de Wit <cyrilwit@gmail.com>
2018-12-08 13:51:32 +0100

    feat: add secure routes

---
+++ routes/web-secure.php
@@ -0,0 +1,30 @@
+<?php
+
+Route::middleware('auth:secure', 'can:browse_secure')->group(function () {
+
+    // Dashboard
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+});
+
+Route::namespace('Auth')->name('auth.')->group(function () {
+
+    // Authentication routes
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('logout');
+
+    // Registration routes
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register');
+
+    // Password reset routes
+    Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.update');
+
+    // Email verification routes
+    Route::get('email/verify', 'VerificationController@showLinkRequestForm')->name('verification.notice');
+    Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
+    Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
+});
