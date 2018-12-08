09b762743d3e821c0dab9464974eb577361b76bb
Cyril de Wit <cyrilwit@gmail.com>
2018-12-08 21:46:19 +0100

    feat: add owners table seeder

---
+++ database/seeds/OwnersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Domain\Owners\Models\Owner;
+
+class OwnersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $john = Owner::create([
+            'name'              => 'Jamy Johnson',
+            'email'             => 'jamyjohnson@example.com',
+            'email_verified_at' => now(),
+            'password'          => bcrypt('pass'),
+        ]);
+
+        $jade = Owner::create([
+            'name'              => 'Jady Smith',
+            'email'             => 'jadymith@example.com',
+            'email_verified_at' => now(),
+            'password'          => bcrypt('pass'),
+        ]);
+    }
+}
