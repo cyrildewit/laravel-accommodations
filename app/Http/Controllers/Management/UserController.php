54e921482c62e76d83c660d04e4742e9a35933c3
Cyril de Wit <cyrilwit@gmail.com>
2018-12-08 13:35:52 +0100

    feat: add UserController for Management

---
+++ app/Http/Controllers/Management/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Management;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * List all users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('management.user.index');
+    }
+
+    /**
+     * Create user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('management.user.create');
+    }
+
+    /**
+     * Store user.
+     *
+     * @param  Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show user.
+     *
+     * @param  \App\Models\Partner  $partner
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('management.user.show');
+    }
+
+    /**
+     * Edit user.
+     *
+     * @param  \App\Models\Partner  $partner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('management.user.edit');
+    }
+
+    /**
+     * Update user.
+     *
+     * @param  Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Destroy user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        //
+    }
+}
