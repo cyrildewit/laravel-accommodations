edf3b58e57623c0fab40bd59b8a2e214e2fe80ea
Cyril de Wit <cyrilwit@gmail.com>
2018-12-09 22:48:51 +0100

    feat: add more bookings to seeder to test availability

--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use App\Domain\Bookings\Models\Booking;
 
@@ -15,9 +16,39 @@ class BookingsTableSeeder extends Seeder
         Booking::create([
             'room_id' => 1,
             'user_id' => 1,
-            'reference_id' => 1300030242,
-            'check_in_date' => now()->today(),
-            'check_out_date' => now()->today()->addDays(4),
+            'reference_id' => '1300030242',
+            'check_in_date' => Carbon::createFromDate(2018, 4, 5),
+            'check_out_date' => Carbon::createFromDate(2018, 4, 6),
+            'number_of_guests' => 2,
+            'total_price' => 90,
+        ]);
+
+        Booking::create([
+            'room_id' => 1,
+            'user_id' => 1,
+            'reference_id' => '2300030242',
+            'check_in_date' => Carbon::createFromDate(2018, 4, 6),
+            'check_out_date' => Carbon::createFromDate(2018, 4, 7),
+            'number_of_guests' => 2,
+            'total_price' => 90,
+        ]);
+
+        Booking::create([
+            'room_id' => 1,
+            'user_id' => 1,
+            'reference_id' => '3300030242',
+            'check_in_date' => Carbon::createFromDate(2018, 4, 8),
+            'check_out_date' => Carbon::createFromDate(2018, 4, 9),
+            'number_of_guests' => 2,
+            'total_price' => 90,
+        ]);
+
+        Booking::create([
+            'room_id' => 1,
+            'user_id' => 1,
+            'reference_id' => '4300030242',
+            'check_in_date' => Carbon::createFromDate(2018, 4, 15),
+            'check_out_date' => Carbon::createFromDate(2018, 4, 20),
             'number_of_guests' => 2,
             'total_price' => 90,
         ]);
