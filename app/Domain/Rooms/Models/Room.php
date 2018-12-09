1155b85aaa5efee02cae5eaf0d45ccbe5e24c465
Cyril de Wit <cyrilwit@gmail.com>
2018-12-09 22:48:32 +0100

    feat: add bookings relationship to Room and add return type

--- app/Domain/Rooms/Models/Room.php
+++ app/Domain/Rooms/Models/Room.php
@@ -4,8 +4,11 @@ namespace App\Domain\Rooms\Models;
 
 use Spatie\EloquentSortable\Sortable;
 use App\Domain\Listings\Models\Listing;
+use App\Domain\Bookings\Models\Booking;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\SortableTrait;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Room extends Model implements Sortable
 {
@@ -35,8 +38,18 @@ class Room extends Model implements Sortable
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function listing()
+    public function listing(): BelongsTo
     {
         return $this->belongsTo(Listing::class);
+    }
+
+    /**
+     * Get the bookings of the room.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
     }
 }
