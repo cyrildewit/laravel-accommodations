e0ebd6c776caf081ed7067400286756e24589a54
Cyril de Wit <cyrilwit@gmail.com>
2018-12-15 17:22:08 +0100

    chore(testing): add RoomsTest

---
+++ tests/Feature/RoomsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use App\Domain\Rooms\Models\Room;
+use App\Domain\Owners\Models\Owner;
+use App\Domain\Listings\Models\Listing;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoomsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @test
+     * @return void
+     */
+    public function availability_for_a_period_can_be_determined()
+    {
+        $owner = factory(Owner::class)->create();
+        $listing = factory(Listing::class)->create(['owner_id' => $owner->getKey()]);
+        $room = factory(Room::class)->create(['listing_id' => $listing->getKey()]);
+
+        // $room->bookings()->saveMany();
+
+        $this->assertTrue($room->isAvailableForPeriod(Carbon::createFromDate(2018, 4, 5), Carbon::createFromDate(2018, 4, 6)));
+    }
+}
