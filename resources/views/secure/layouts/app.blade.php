8ffeba95a2ea4d820e7da93ff0f73706ad4cd77d
Cyril de Wit <cyrilwit@gmail.com>
2019-07-10 17:10:26 +0200

    refactor: clean up app layout

--- resources/views/secure/layouts/app.blade.php
+++ resources/views/secure/layouts/app.blade.php
@@ -3,11 +3,7 @@
 <head>
     @include('secure.layouts.partials.head.meta')
 
-    <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ mix('css/secure.css') }}">
 
