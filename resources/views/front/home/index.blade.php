d4a8232db092ad13b4f81ee099046fa379322a99
Cyril de Wit <cyrilwit@gmail.com>
2019-07-10 15:23:50 +0200

    refactor: remove test code

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -1,18 +1,4 @@
 @component('front.layouts.main', [
     // 'meta' => $meta,
 ])
-<h1>Homepage</h1>
-
-@if(Auth::guard('management')->user()->hasRole('super-admin'))
-<h2>I am a super admin</h2>
-@else
-<h2>I am NOT a super admin</h2>
-@endif
-
-@can('add_listings')
-<h2>I can add listings</h2>
-@else
-<h2>I CAN'T add listings</h2>
-@endcan
-
 @endcomponent
