e963a00b98ac84c737eb87a891298be28994c980
Cyril de Wit <cyrilwit@gmail.com>
2019-07-25 16:07:00 +0200

    feat: show all listings on search page

--- app/App/Http/Controllers/Front/SearchController.php
+++ app/App/Http/Controllers/Front/SearchController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Front;
 
+use Domain\Listing\Models\Listing;
 use App\Http\Controllers\Controller;
 
 class SearchController extends Controller
 {
     public function index()
     {
-        $listings = Listing::all();
+        $listings = Listing::paginate(15);
 
         return view('front.search.index', [
-            $matchedListings = $listings,
+            'listings' => $listings,
         ]);
     }
 }
