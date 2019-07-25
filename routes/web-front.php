249ee38fcf5e6903fd40b18973b0c6b49b483f65
Cyril de Wit <cyrilwit@gmail.com>
2019-07-25 16:09:42 +0200

    refactor: improve controller binding

--- routes/web-front.php
+++ routes/web-front.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Auth;
 use Domain\Listing\Models\Listing;
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\ListingController;
+use App\Http\Controllers\Front\SearchController;
 
 // Auth::loginUsingId(1);
 // Auth::guard('management')->loginUsingId(1);
 
-Route::get('/', 'HomeController@index')->name('home.index');
-Route::get('/search', 'SearchController@index')->name('search.index');
-Route::get('/listings/{id}', 'ListingController@show')->name('listing.show');
-// Route::get('/{listingType}/{areaSlug}/{listingSlug}/{id}', 'ListingController@show')->name('listing.show');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/search', [SearchController::class, 'index'])->name('search.index');
+Route::get('/listings/{id}', [ListingController::class, 'show'])->name('listing.show');
+// Route::get('/{listingType}/{areaSlug}/{listingSlug}/{id}', [ListingController::class, 'show'])->name('listing.show');
