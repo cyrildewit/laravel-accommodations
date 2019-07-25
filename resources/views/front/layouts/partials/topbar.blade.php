8bda9b9131765c163aabb2c53732522e9b76e5b4
Cyril de Wit <cyrilwit@gmail.com>
2019-07-25 16:12:37 +0200

    feat: update topbar

--- resources/views/front/layouts/partials/topbar.blade.php
+++ resources/views/front/layouts/partials/topbar.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container">
 
-        <a class="navbar-brand" href="#">Accommodations</a>
+        <a class="navbar-brand" href="{{ route('front.home.index') }}">Accommodations</a>
 
         <!-- Sidebar Toggle (Topbar) -->
         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -13,33 +13,47 @@
         <!-- Topbar Navbar -->
         <ul class="navbar-nav ml-auto">
 
+            {{-- <div class="topbar-divider d-none d-sm-block"></div> --}}
+
+            @guest('secure')
+            <!-- Nav Item - Login -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('secure.auth.login') }}">
+                    <span class="d-none d-lg-inline text-gray-600">Inloggen</span>
+                </a>
+            </li>
+            @endguest
+
+            @auth('secure')
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-            </a>
-            <!-- Dropdown - User Information -->
-            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                Profile
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ current_user()->full_name }}</span>
+                    <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
                 </a>
-                <a class="dropdown-item" href="#">
-                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                Logout
-                </a>
-            </div>
+
+                <!-- Dropdown - User Information -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Profile
+                    </a>
+                    <a class="dropdown-item" href="#">
+                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Settings
+                    </a>
+                    <a class="dropdown-item" href="#">
+                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Activity Log
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                    </a>
+                </div>
             </li>
+            @endauth
 
         </ul>
 
