f72287c5945f6b850ce8a1d85e2afb72bf69d16d
Cyril de Wit <cyrilwit@gmail.com>
2019-07-26 19:43:54 +0200

    feat: update login page design to use template

--- resources/views/secure/auth/login.blade.php
+++ resources/views/secure/auth/login.blade.php
@@ -1,68 +1,75 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@component('front.layouts.app', [
+    // 'meta' => $meta,
+    'body_class' => 'bg-gradient-primary d-flex flex-row align-items-center'
+])
+<div class="container">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/c2c562ce07.js"></script>
-</head>
-<body class="bg-light d-flex flex-row align-items-center">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
+        <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card p-4">
-                    <div class="card-body">
-                        <h2>Login</h5>
-                        <p class="text-muted">Secured portal for users who book listings</p>
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
 
-                        <form method="POST" action="{{ route('secure.auth.login') }}">
-
-                            {{ csrf_field() }}
-
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <i class="fas fa-envelope"></i>
-                                    </span>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                 </div>
 
-                                <input class="form-control" type="email" name="email" placeholder="E-mailaddress">
+                                <form method="POST" action="{{ route('secure.auth.login') }}" class="user">
+                                    {{ csrf_field() }}
 
+                                    <div class="form-group">
+                                        <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <label class="custom-control-label" for="customCheck">Remember Me</label>
+                                        </div>
+                                    </div>
+
+                                    <button class="btn btn-primary btn-user btn-block" type="submit">
+                                        Login
+                                    </button>
+
+                                    {{-- <hr>
+                                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <i class="fab fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    </a> --}}
+                                </form>
+
+                                <hr>
+
+                                <div class="text-center">
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="small" href="register.html">Create an Account!</a>
+                                </div>
                             </div>
-
-                            <div class="input-group mb-4">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <i class="fas fa-unlock-alt"></i>
-                                    </span>
-                                </div>
-
-                                <input class="form-control" type="password" name="password" placeholder="Password">
-
-                            </div>
-
-                            <div class="row">
-                                <div class="col-6">
-                                    <button class="btn btn-primary px-4" type="submit">Login</button>
-                                </div>
-
-                                <div class="col-6 text-right">
-                                    <a class="btn btn-link px-0" href="">Forgot password?</a>
-                                </div>
-                            </div>
-
-                        </form>
-
+                        </div>
                     </div>
                 </div>
-
             </div>
+
         </div>
+
     </div>
 
-</body>
-</html>
+  </div>
+@endcomponent
