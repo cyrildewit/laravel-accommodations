14991049ee9f41317c427ffb0f1c5ee978c57a1d
Cyril de Wit <cyrilwit@gmail.com>
2019-07-26 20:23:18 +0200

    feat: cast type attribute to ListingType enum

--- resources/views/front/search/index.blade.php
+++ resources/views/front/search/index.blade.php
@@ -53,7 +53,7 @@
 
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title"><span class="badge badge-secondary">New</span>{{ $listing->name }}</h5>
+                            <h5 class="card-title"><span class="badge badge-secondary">{{ $listing->type->description }}</span> {{ $listing->name }}</h5>
                             <p class="card-text small">{{ $listing->description }}</p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
