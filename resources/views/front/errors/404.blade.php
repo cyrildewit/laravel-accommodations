104e7250402057b808756772071cfc26fa0c6701
Cyril de Wit <cyrilwit@gmail.com>
2019-07-29 10:37:35 +0200

    feat: update 404 error page design of front

--- resources/views/front/errors/404.blade.php
+++ resources/views/front/errors/404.blade.php
@@ -1,1 +1,11 @@
-<h1>Error 404: Page Not Found</h1>
+@component('front.layouts.main', [
+    // 'meta' => $meta,
+])
+<!-- 404 Error Text -->
+<div class="text-center py-5">
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+    <a href="{{ route('front.home.index') }}">&larr; Back to Home</a>
+</div>
+@endcomponent
