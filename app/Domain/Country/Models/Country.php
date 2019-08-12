4d6a3b55266bb6386d23da642a2c76c94d53cd38
Cyril de Wit <cyrilwit@gmail.com>
2019-08-12 13:51:30 +0200

    feat: add countries table

---
+++ app/Domain/Country/Models/Country.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domain\Country\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'countries';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
